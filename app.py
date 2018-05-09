1c4016df5ce8646a8d8c9f2f208a0d56dc8c0d8a
manelmahroug <manel.khan@gmail.com>
2018-05-09 13:22:18 -0700

    added a file

--- app.py
+++ app.py
@@ -1,33 +1,39 @@
 from flask import Flask, render_template, jsonify, redirect
+from flask_pymongo import PyMongo
 import pymongo
 import scrape_mars
 
+ 
+
 app = Flask(__name__)
 
-
-
 conn = 'mongodb://localhost:27017'
-client = pymongo.MongoClient(conn)
-db = client.current_mars_info
+client = pymongo.MongoClient(conn) 
+mars_db=client.marsScrape_DB
 collection = db.mars_info
+
 
 @app.route("/")
 def index():
-    mars = mongo.db.mars_info.find_one()
-    return render_template("index.html", mars = mars)
+    marsScrape_Data = db.mars_info.find_one()
+    return render_template("index.html", mars=mars)
 
 
 @app.route("/scrape")
 def scrape():
-    mars = mongo.db.mars_info
-    data = scrape_mars.scrape()
-    mars.update(
+    mars_data = scrape_mars.scrape()
+    marsDb = mars_db.mars_info
+    marsDb.update(
         {},
-        data,
+        mars_data,
         upsert=True
     )
+
+
     return redirect("http://localhost:5000/", code=302)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+ 
